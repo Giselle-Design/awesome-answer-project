ecac982e3fa841c3938510834cd15f55035f8681
Hano <hinsul3113@gmail.com>
2020-07-09 11:50:57 -0700

    Add notes for querying through DB using active record

--- app/models/question.rb
+++ app/models/question.rb
@@ -32,5 +32,51 @@ class Question < ApplicationRecord
     # you can create a record right away in the DB using (.create) mthod as in:
     # Question.create({ title: "My Question Title ", body: "My question body", view_count: 0 })
 
+    # Fetching Records 
+    # .first 
+    # Question.first 👈 fetches the first record ordered by the id in an ascending order 
+    # The sql equivalent:
+    # SELECT "questions".* FROM "questions" ORDER BY "questions"."id" ASC LIMIT 1;
+
+    # .last
+    # Question.last 👈 fetches the last record ordered by the id in an ascending order 
+    # The sql equivalent:
+    # SELECT "questions".* FROM "questions" ORDER BY "questions"."id" DESC LIMIT 1;
+
+    # .find
+    # Question.find(1) 👈 finding records by their primary key which is likely id
+
+    # .find_by_x which x is the column name
+    # Question.find_by_title("One more Question") 👈 this finds a question with title
+    # exactly "One more Question"
+
+    # .where
+    # Question.where( {title: "My Question Title", body: "My question body" })
+
+    # WILDCARD SEARCHING 
+    # You can perform wildcard searching with ActiveRecord using LIKE within where method
+    # Question.where(['title LIKE ?', '%more%'])
+    # if you are using Postgres client, you can use ILIKE for case-insensitive searching:
+    # Question.where(['title ILIKE ?', '%more%'])
+    # Note that in above queries we used a syntax that uses '?' to insert variable into a SQL
+    # query. this is important to avoid having SQL injection if the variable component is 
+    # actually a suer input such as search term.
+
+    # .limit 
+    # Question.limit(10) 👈 this will give us back 10 first question that are returned 
+    # from the query
+
+    # .order 
+    # Question.order(:created_at) 👈 this will order the fetched records by 
+    # created_at by default in ASC and if you want them in descending order do:
+    # Question.order(created_at: :DESC)
+
+    # Chaining
+    # you can chain the where, limit, order, offset, and many others to compose
+    # more sophisticated queries for example:
+    # Question.where(['view_count > ?', 10]).where(['title ILIKE ?', '%a%']).order(id: :DESC).limit(10).offset(10)
+    # Note: offset will skip first 10 records 
+    # SELECT "questions".* FROM "questions" WHERE (view_count > 10) AND (title ILIKE '%a%') ORDER BY id DESC LIMIT 10 OFFSET 10;
+
     
 end
