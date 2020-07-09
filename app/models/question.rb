3b7f7b9c840c4b87993d144d912bf60d854a9757
Hano <hinsul3113@gmail.com>
2020-07-09 11:00:48 -0700

    add faker and generate seeds for questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,10 @@
 class Question < ApplicationRecord
+    # This is the Question model. We generate this 
+    # file with the command:
+    # rails g model question title:string body:text
+    # This command also genrates a migration file 
+    # in db/migrate
+
+    # Rails will add attr_accessors for all columns 
+    # of the table (i.e. title, body, created_at, updated_at, view_count)
 end
