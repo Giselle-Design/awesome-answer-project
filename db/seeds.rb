a7e3bd6a1a3fd5ffbcc41164a00a29e9a0dc2282
Hano <hinsul3113@gmail.com>
2020-07-14 11:37:59 -0700

    Add the list of answers to question show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,30 @@
 
 # To run your seeds, do:
 # rails db:seed
-
+Answer.delete_all
 Question.delete_all
 
 NUM_QUESTIONS = 200
 
 NUM_QUESTIONS.times do 
     created_at = Faker::Date.backward(days: 365)
-    Question.create(
+    q = Question.create(
         title: Faker::Hacker.say_something_smart,
         body: Faker::ChuckNorris.fact,
         created_at: created_at, 
         updated_at: created_at,
         view_count: Faker::Number.between(from: 1, to: 100)
     )
+
+    if q.valid?
+        q.answers = rand(0..15).times.map do 
+            Answer.new(body: Faker::GreekPhilosophers.quote)
+        end
+    end
 end
 
 question = Question.all
+answer = Answer.all
 
 puts Cowsay.say("Generated #{question.count} questions", :frogs)
+puts Cowsay.say("Generated #{answer.count} answers", :tux)
