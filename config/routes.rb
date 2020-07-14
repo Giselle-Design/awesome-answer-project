99f649cbc150d91e8b71bae708ed4c623fecf8ea
Hano <hinsul3113@gmail.com>
2020-07-14 10:42:06 -0700

    Nest answers routes within questions

--- config/routes.rb
+++ config/routes.rb
@@ -27,27 +27,49 @@ Rails.application.routes.draw do
   # question new page
   # when someone sends a GET request to /questions/new, it will be handled by the new actions 
   # inside questions controller
-  get '/questions/new', {to: 'questions#new', as: :new_questions }
+  # get '/questions/new', {to: 'questions#new', as: :new_questions }
 
-  # handles submission of new questions form
-  post '/questions', {to: 'questions#create', as: :questions }
+  # # handles submission of new questions form
+  # post '/questions', {to: 'questions#create', as: :questions }
   
-  # question show page
-  get '/questions/:id', { to: 'questions#show', as: :question }
+  # # question show page
+  # get '/questions/:id', { to: 'questions#show', as: :question }
 
-  # questions index page
-  get '/questions', { to: 'questions#index' }
+  # # questions index page
+  # get '/questions', { to: 'questions#index' }
 
-  # question edit page
-  get '/questions/:id/edit', { to: 'questions#edit', as: :edit_question}
+  # # question edit page
+  # get '/questions/:id/edit', { to: 'questions#edit', as: :edit_question}
   
-  # handles submission of form on the question edit page
-  patch '/questions/:id', { to: 'questions#update' }
+  # # handles submission of form on the question edit page
+  # patch '/questions/:id', { to: 'questions#update' }
 
-  # delete a question
-  delete '/questions/:id', { to: 'questions#destroy' }
+  # # delete a question
+  # delete '/questions/:id', { to: 'questions#destroy' }
 
+  # get "/questions/faq", to: "questions#faq"
+  
   # Below line builds all of the above routes for us :)
-  # resources :questions
+  # resources :questions  build the CRUD RESTful routes. (all of the above 
+  #  routes for questions)
+  # 'resources' method will generate all CRUD routes 
+  # following RESTful conventions for a resource
+  # It will assume there is a controller named after the
+  # first argument, pluralized and PascalCase
+  resources :questions do 
+    # Routes written inside of a block passed to a resource
+    # method will be pre-fixed by a path corresponding
+    # to the passed in symbol.
+    # In this case, all nested routes will be pre-fixed 
+    # with '/questions/:question_id'
+    resources :answers, only: [:create, :destroy]
+    # equivalent to:
+    # resources :answers, except: [:show, :index, :new, :edit, :update]
+    # questions_answers_path(<question_id>)
+    # question_answer_url(<question_id>)
+    # question_answers_path(@question)
+  end
+
+  
 
 end
