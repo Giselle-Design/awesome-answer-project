63c39e745945cb33648f7cf28165f790f7decc8a
Hano <hinsul3113@gmail.com>
2020-07-15 10:01:48 -0700

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
     has_many :questions, dependent: :nullify
     has_many :answers, depenedent: :nullify
+
+    VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX
+
+    def full_name 
+        "#{first_name} #{last_name}"
+    end
 end
