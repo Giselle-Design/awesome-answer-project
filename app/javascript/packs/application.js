b5d30f6136f521cd9f7e7702e3c092e060f52a47
Hano <hinsul3113@gmail.com>
2020-07-16 11:15:54 -0700

    Add bootstrap and move stylesheets to packs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,22 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// This file is the entry point for webpack, which means webpack first look in this
+// file. So, the 'application' pack is the entry point of all of our JavaScript files
+// We can create custom packs and palce them in the app/javascript/packs directory
+// and webpacker will find them easily while compiling. and this setting is
+// configured in 'config/webpacker.yml'.
+
+// Rails frameworks which have JavaScript components such as Rails-UJS,
+// Turbolinks, ActiveStorage are migrated to support webpacker
+
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import "bootstrap"
+import "../stylesheets/application"
+import "./question"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -14,3 +26,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+console.log("Hello from app/javascript/packs/application.js")
